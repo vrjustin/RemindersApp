e3906b412bb551b3a0b8ae9dd91dc81ada8b3b51
Justin Maronde <myemailisjustin@me.com>
2024-10-27 05:57:28 -0400

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Justin Maronde on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
